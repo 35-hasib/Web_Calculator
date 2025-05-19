17e00ac2ceaed272cbb2c3eec90f350201ebc560
35-hasib <hrrahman35@gmail.com>
2025-05-19 08:30:31 +0600

    Fixed Leading Zeros issue

--- test.js
+++ test.js
@@ -2,12 +2,12 @@ let inputBox = document.getElementById('inputBox')
 let buttons = document.querySelectorAll('button')
 
 let string = ''
+let ResultShowingFlag = 0
 
 buttons.forEach(e => {
-    ResultShowingFlag = 0
     e.addEventListener('click', (b) => {
         
-        key = b.target.innerText
+        let key = b.target.innerText
 
         if (key == '=') {
             string = String(Math.round(eval(string) * 1000) / 1000) // Round to 3 decimal places
@@ -23,16 +23,29 @@ buttons.forEach(e => {
             inputBox.value = string
         }
         else {
-            if (ResultShowingFlag == 0) {
-                string += key
-                inputBox.value = string
-            }
-            else {
+            if (ResultShowingFlag == 1) {
                 string = ''
-                string += key
-                inputBox.value = string
                 ResultShowingFlag = 0
             }
+
+            if (key.match(/[0-9]/)) {
+                if (/[+\-*/]$/.test(string) && key === '0') {
+                    if (!string.endsWith('0')) {
+                        string += key
+                    }
+                }
+                else if (string === '0' || string === '00') {
+                    string = key
+                }
+                else {
+                    string += key
+                }
+            }
+            else {
+                string += key
+            }
+
+            inputBox.value = string.replace(/([+\-*/])0+/g, '$10')
         }
     })
 })
